f9882794b1eff80fca5b4123d27af5e3104e2012
spoorthyv <sckortyman@gmail.com>
2017-08-12 19:07:35 -0700

    Start styling explore view

--- src/components/Home/ExploreView.js
+++ src/components/Home/ExploreView.js
@@ -5,7 +5,18 @@ export default class ExploreView extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>ExploreView</Text>
+        <View style={styles.headerContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Explore</Text>
+          </View>
+          <View style={styles.arrowContainer}>
+            <Text style={styles.arrow}>></Text>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+          </View>
+        </View>
       </View>
     );
   }
@@ -13,9 +24,41 @@ export default class ExploreView extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#6F49FF',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+  },
+  titleContainer: {
     flex: 1,
+    flexDirection: 'row',
+    padding: 15,
+  },
+  title: {
+    fontWeight: "700",
+    fontSize: 24,
+    marginLeft: 15,
+    color: '#fff',
+  },
+  arrowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: 15,
+  },
+  arrow: {
+    fontWeight: "700",
+    fontSize: 24,
+    color: '#fff',
+    marginRight: 15,
+    transform: [{rotateZ: '90deg'}],
+  },
+  cardContainer: {
+    flexDirection: 'row',
+  },
+  card: {
+    height: 230,
+    width: 175,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
